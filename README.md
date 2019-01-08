08fb9fe45771aa2e3a8b40e752bd7995b35f4033
Giymo11 <gizmo.head@yahoo.de>
2019-01-08 10:20:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Knuckles Kalaha Agent
+
+- Edit Configurations. 
+- Press on the "+" symbol and select Application. 
+- The main class should be at.pwd.boardgame.Main
+- The Program arguments should be the full classname (including the package), so `Knuckles`.
+- The "Use classpath of module" box should be `knuckles.main` or similar.
---
+++ src/main/java/Knuckles.java
@@ -0,0 +1,17 @@
+
+import at.pwd.boardgame.game.mancala.MancalaGame;
+import at.pwd.boardgame.game.mancala.agent.MancalaAgent;
+import at.pwd.boardgame.game.mancala.agent.MancalaAgentAction;
+
+public class Knuckles implements MancalaAgent {
+
+  @Override
+  public MancalaAgentAction doTurn(int i, MancalaGame mancalaGame) {
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return "Knuckles";
+  }
+}
