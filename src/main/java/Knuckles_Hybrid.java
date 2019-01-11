6a48aec16152733a1128a3bf6f19f2ad0fe8064c
Michael Reitgruber <reitgruber.michael@gmail.com>
2019-01-11 17:38:52 +0100

    primitive check to find wheter we are near the end of the game

---
+++ src/main/java/Knuckles_Hybrid.java
@@ -0,0 +1,149 @@
+
+import at.pwd.boardgame.game.base.Game;
+import at.pwd.boardgame.game.base.WinState;
+import at.pwd.boardgame.game.mancala.MancalaGame;
+import at.pwd.boardgame.game.mancala.MancalaState;
+import at.pwd.boardgame.game.mancala.agent.MancalaAgent;
+import at.pwd.boardgame.game.mancala.agent.MancalaAgentAction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static sun.java2d.cmm.ColorTransform.In;
+
+@SuppressWarnings("Duplicates")
+public class Knuckles_Hybrid implements MancalaAgent {
+  private Random r = new Random();
+  private MancalaState originalState;
+  private static final double C = 1.0f/Math.sqrt(2.0f);
+  private static final int ENDGAME = 12; //Test to find the best value for this
+
+  private class MCTSTree {
+    private int visitCount;
+    private int winCount;
+
+    private MancalaGame game;
+    private WinState winState;
+    private MCTSTree parent;
+    private List<MCTSTree> children;
+    String action;
+
+    MCTSTree(MancalaGame game) {
+      this.game = game;
+      this.children = new ArrayList<>();
+      this.winState = game.checkIfPlayerWins();
+    }
+
+    boolean isNonTerminal() {
+      return winState.getState() == WinState.States.NOBODY;
+    }
+
+    MCTSTree getBestNode() {
+      MCTSTree best = null;
+      double value = 0;
+      for (MCTSTree m : children) {
+        double wC = (double)m.winCount;
+        double vC = (double)m.visitCount;
+        double currentValue =  wC/vC + C*Math.sqrt(2*Math.log(visitCount) / vC);
+
+
+        if (best == null || currentValue > value) {
+          value = currentValue;
+          best = m;
+        }
+      }
+
+      return best;
+    }
+
+    boolean isFullyExpanded() {
+      return children.size() == game.getSelectableSlots().size();
+    }
+
+    MCTSTree move(String action) {
+      MancalaGame newGame = new MancalaGame(this.game);
+      if (!newGame.selectSlot(action)) {
+        newGame.nextPlayer();
+      }
+
+      MCTSTree tree = new MCTSTree(newGame);
+      tree.action = action;
+      tree.parent = this;
+
+      this.children.add(tree);
+
+      return tree;
+    }
+  }
+
+
+  @Override
+  public MancalaAgentAction doTurn(int computationTime, MancalaGame game) {
+    long start = System.currentTimeMillis();
+    this.originalState = game.getState();
+
+    MCTSTree root = new MCTSTree(game);
+
+    while ((System.currentTimeMillis() - start) < (computationTime*1000 - 100)) {
+      MCTSTree best = treePolicy(root);
+      WinState winning = defaultPolicy(best.game);
+      backup(best, winning);
+    }
+
+    MCTSTree selected = root.getBestNode();
+    System.out.println("Selected action " + selected.winCount + " / " + selected.visitCount);
+    return new MancalaAgentAction(selected.action);
+  }
+
+  private void backup(MCTSTree current, WinState winState) {
+    boolean hasWon = winState.getState() == WinState.States.SOMEONE && winState.getPlayerId() == originalState.getCurrentPlayer();
+
+    while (current != null) {
+      // always increase visit count
+      current.visitCount++;
+
+      // if it ended in a win => increase the win count
+      current.winCount += hasWon ? 1 : 0;
+
+      current = current.parent;
+    }
+  }
+
+  private MCTSTree treePolicy(MCTSTree current) {
+    while (current.isNonTerminal()) {
+      if (!current.isFullyExpanded()) {
+        return expand(current);
+      } else {
+        current = current.getBestNode();
+      }
+    }
+    return current;
+  }
+
+  private MCTSTree expand(MCTSTree best) {
+    List<String> legalMoves = best.game.getSelectableSlots();
+    return best.move(legalMoves.get(r.nextInt(legalMoves.size())));
+  }
+
+  private WinState defaultPolicy(MancalaGame game) {
+    return DefaultPolicies.random(game);
+  }
+
+  @Override
+  public String toString() {
+    return "Knuckles Hybrid";
+  }
+
+  private boolean isNearTheEnd(MancalaGame g) {
+    String p1Depot = g.getBoard().getDepotOfPlayer(1);
+    String p2Depot = g.getBoard().getDepotOfPlayer(2);
+
+    int p1DepotStones = g.getState().stonesIn(p1Depot);
+    int p2DepotStones = g.getState().stonesIn(p2Depot);
+
+    int stonesInPlay = 72  - (p1DepotStones + p2DepotStones);
+
+    return stonesInPlay <= ENDGAME; //if we have less stones than a certain value, we are in the endgame
+  }
+}
